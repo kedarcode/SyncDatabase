3dfe8155756a493b0b02e9d98a93fa72f151477d
kedar shelar <98396591+kedarcode@users.noreply.github.com>
2022-08-13 00:21:53 +0530

    push for prod

--- syncdatabase.py
+++ syncdatabase.py
@@ -1,4 +1,7 @@
+import ast
 import pymongo
+import requests
+import json
 
 
 def createConn(client, database, collection):
@@ -8,28 +11,31 @@ def createConn(client, database, collection):
     return mycol
 
 
-db1 = createConn("mongodb://localhost:27017/", "syncdatabae1", "items")
+db1 = requests.get('http://siddhiartjewellery.true-order.com/WebReporter/api/v1/items', headers={'Content-Type': ''
+                                                                                                                 'application/json;charset=UTF-8',
+                                                                                                 'X-Auth-Token': '20D48FEA0700964D5FF5A51519F925D464D862798609188500AB67277A82E796FE7A104D6C57DDAB'})
+data1 = db1.json()['items']
 
 db2 = createConn("mongodb://localhost:27017/", "syncdatabae2", "items2")
-
-data1 = db1.find()
-
 for item in data1:
     itemid = item['itemId']
     for stock in item['stock']:
-
         target_query = {"itemId": itemid, "refId": stock['itemReferenceCode']}
         target = db2.find_one(target_query)
-
         if target is None:
-            item['stock'] = stock
-            item['refId'] = stock['itemReferenceCode']
-            db2.insert_one(item)
-            print('missing', stock["stock"], {"itemId": itemid, "refId": stock['itemReferenceCode']})
+            try:
+                item['stock'] = stock
+                item['refId'] = stock['itemReferenceCode']
+                #db2.insert_one(item)
+                print('------',item)
+            except:
+                continue
 
         elif stock["stock"] != target['stock']['stock']:
-            print('stcokchhange', stock["stock"], target['stock']['stock'],
-                  {"itemId": itemid, "refId": stock['itemReferenceCode']})
-            target['stock']['stock']=stock['stock']
-            newvalues = {"$set": {"stock": target['stock']}}
-            x = db2.update_one(target_query, newvalues)
+            try:
+                target['stock']['stock'] = stock['stock']
+                newvalues = {"$set": {"stock": target['stock']}}
+                print(target,newvalues)
+               # x = db2.update_one(target_query, newvalues)
+            except:
+                continue
